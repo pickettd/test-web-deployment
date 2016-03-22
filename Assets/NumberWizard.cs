12733cefd572e8b8b100d4619bd170eaba524e09
David Pickett <pickettd@gmail.com>
2016-03-22 11:48:45 -1000

    Added files via upload

---
+++ Assets/NumberWizard.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class NumberWizard : MonoBehaviour {
+	int max;
+	int min;
+	int guess;
+	public int maxGuessesAllowed = 9;
+	public Text maxGuessString;
+
+	public Text text;
+
+	void Guess () {
+		//guess = Random.Range(min, max+1);
+		guess = (max+min) / 2;
+		text.text = guess.ToString();
+		maxGuessesAllowed -= 1;
+		maxGuessString.text = maxGuessesAllowed.ToString();
+		if (maxGuessesAllowed == 0) {
+			Application.LoadLevel("Win");
+		}
+	}
+	
+	// Use this for initialization
+	void Start () {
+		max = 1000;
+		min = 1;
+		//guess = Random.Range(min, max+1);
+		guess = 500;
+		text.text = guess.ToString();
+		maxGuessString.text = maxGuessesAllowed.ToString();
+		max += 1;
+	}
+
+	public void GuessHigher() {
+		min = guess;
+		Guess ();
+	}
+
+	public void GuessLower() {
+		max = guess;
+		Guess ();
+	}
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# test-web-deployment
+# number-wizard-ui-unity-course
+Number Wizard UI Unity4 Game for Udemy Unity Course
+
+Unity Web Player demo available at http://pickettd.github.io/number-wizard-ui-unity-course/ (note that Unity Web Player requires a browser plugin and currently the plugin will not work with Chrome or Microsoft Edge browsers)
